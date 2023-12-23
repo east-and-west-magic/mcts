d51ee2ae954343d64252ae2f84cc0a588bcb8d21
Amy <45153739+amtang325@users.noreply.github.com>
2023-12-22 21:00:19 -0500

    node class

--- mcts/node.py
+++ mcts/node.py
@@ -1,10 +1,19 @@
 class Node:
     def __init__(self, board, parent, player):
         self.board = board
-        self.children = []
+        self.player = player
         self.parent = parent
+        self.children = []
         self.wins = 0
         self.visits = 0
-        self.player = player
+
+    def update_wins(self):
+        self.wins += 1
+
+    def update_visits(self):
+        self.visits += 1
+    
+    def add_child(self, child):
+        self.children.append(child)
 
     
