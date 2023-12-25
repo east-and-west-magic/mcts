ce7abb9fb64e7d913cc14ccb809812e909296755
Amy <45153739+amtang325@users.noreply.github.com>
2023-12-24 22:59:24 -0500

    added partial mcts tree 1

--- mcts/node.py
+++ mcts/node.py
@@ -1,13 +1,13 @@
 import chess
 
 class Node:
-    def __init__(self, board, parent, player):
+    def __init__(self, board, parent, player, wins, visits):
         self.board = board
         self.player = player
         self.parent = parent
         self.children = []
-        self.wins = 0
-        self.visits = 0
+        self.wins = wins
+        self.visits = visits
 
     def __str__(self):
         return f"Board: {self.board.fen()}, Visits: {self.visits}, Wins: {self.wins}, Player: {'White' if self.player else 'Black'}"
--- mcts/tests.py
+++ mcts/tests.py
@@ -6,18 +6,59 @@ from mcts import MonteCarloTreeSearch
 
 
 def main():
-    root_node = Node(chess.Board(), None, chess.WHITE)
-    print(root_node.is_leaf())
-    child_node = Node(chess.Board(), root_node, chess.BLACK)
-    child_node_2 = Node(chess.Board(), root_node, chess.BLACK)
-    root_node.add_child(child_node)
-    root_node.add_child(child_node_2)
-    # print(user)
-    print(root_node.is_leaf())
-    # print(root_node)
-    monte_carlo = MonteCarloTreeSearch(root_node, 2)
-    monte_carlo.expansion(root_node)
-    print(monte_carlo.selection())
+    if False:
+        root_node = Node(chess.Board(), None, chess.WHITE, 0, 0)
+        child_node = Node(chess.Board(), root_node, chess.BLACK)
+        child_node_2 = Node(chess.Board(), root_node, chess.BLACK)
+        root_node.add_child(child_node)
+        root_node.add_child(child_node_2)
+        monte_carlo = MonteCarloTreeSearch(root_node, 2)
+        monte_carlo.expansion(root_node)
+        monte_carlo.selection()
+    
+    # Tree 1
+    if True:
+        root_node = Node(chess.Board("8/4bk1p/2p5/p1p1p3/q1P1P1Qp/2P1BP2/6K1/8 w - - 0 42"), None, chess.WHITE, 40, 100)
+
+        child_node1 = Node(chess.Board("2Q5/4bk1p/2p5/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 1 42"), root_node, chess.BLACK, 20, 50)
+        root_node.add_child(child_node1)
+        child_node2 = Node(chess.Board("8/4bkQp/2p5/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 1 42"), root_node, chess.BLACK, 20, 50)
+        root_node.add_child(child_node2)
+
+        child_node3 = Node(chess.Board("2Q5/4b2p/2p3k1/p1p1p3/q1P1P2p/2P1BP2/6K1/8 w - - 2 43"), child_node1, chess.WHITE, 15, 30)
+        child_node1.add_child(child_node3)
+        child_node4 = Node(chess.Board("2Q5/4b2p/2p2k2/p1p1p3/q1P1P2p/2P1BP2/6K1/8 w - - 2 43"), child_node1, chess.WHITE, 5, 20)
+        child_node1.add_child(child_node4)
+        child_node5 = Node(chess.Board("8/4b1kp/2p5/p1p1p3/q1P1P2p/2P1BP2/6K1/8 w - - 0 43"), child_node2, chess.WHITE, 10, 25)
+        child_node2.add_child(child_node5)
+        child_node6 = Node(chess.Board("8/4b1Qp/2p1k3/p1p1p3/q1P1P2p/2P1BP2/6K1/8 w - - 2 43"), child_node2, chess.WHITE, 10, 25)
+        child_node2.add_child(child_node6)
+
+        child_node7 = Node(chess.Board("6Q1/4b2p/2p3k1/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 3 43"), child_node3, chess.BLACK, 5, 15)
+        child_node3.add_child(child_node7)
+        child_node8 = Node(chess.Board("5Q2/4b2p/2p3k1/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 3 43"), child_node3, chess.BLACK, 10, 15)
+        child_node3.add_child(child_node8)
+        child_node9 = Node(chess.Board("6Q1/4b2p/2p2k2/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 3 43"), child_node4, chess.BLACK, 3, 10)
+        child_node4.add_child(child_node9)
+        child_node10 = Node(chess.Board("5Q2/4b2p/2p2k2/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 3 43"), child_node4, chess.BLACK,2, 10)
+        child_node4.add_child(child_node10)
+        child_node11 = Node(chess.Board("8/4b1kp/2p5/p1p1p1B1/q1P1P2p/2P2P2/6K1/8 b - - 1 43"), child_node5, chess.BLACK, 5, 15)
+        child_node5.add_child(child_node11)
+        child_node12 = Node(chess.Board("8/4b1kp/2p5/p1B1p3/q1P1P2p/2P2P2/6K1/8 b - - 0 43"), child_node5, chess.BLACK, 5, 10)
+        child_node5.add_child(child_node12)
+        child_node13 = Node(chess.Board("6Q1/4b2p/2p1k3/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 3 43"), child_node6, chess.BLACK, 6, 15)
+        child_node6.add_child(child_node13)
+        child_node14 = Node(chess.Board("5Q2/4b2p/2p1k3/p1p1p3/q1P1P2p/2P1BP2/6K1/8 b - - 3 43"), child_node6, chess.BLACK, 4, 10)
+        child_node7.add_child(child_node14)
+
+        monte_carlo = MonteCarloTreeSearch(root_node, 2)
+
+
+
+    
+    # Tree 2
+    if False:
+        root_node
     
 
 if __name__ == "__main__":
