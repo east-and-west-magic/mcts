ceddd9829d812d841e823244dfb2766a493108b3
Steve <stevez@playgo.ai>
2024-01-23 00:35:23 +0800

    clean up

--- mcts/node.py
+++ mcts/node.py
@@ -1,5 +1,3 @@
-import chess
-
 class Node:
     def __init__(self, board, parent, player, wins, visits, move):
         self.board = board
@@ -10,23 +8,32 @@ class Node:
         self.visits = visits
         self.move = move
 
+
     def __str__(self):
         return f"Board: {self.board.fen()}, Visits: {self.visits}, Wins: {self.wins}, Player: {'White' if self.player else 'Black'}"
+
 
     def update_wins(self, value):
         self.wins += value
 
+
     def update_visits(self):
         self.visits += 1
-    
+
+
     def add_child(self, child):
         self.children.append(child)
+
 
     def is_leaf(self):
         return not self.children
     
+    
     def path(self):
-        ###################### 
+        """
+        get all the parent nodes based on the current node.
+        The output will be root->...->current_node.
+        """
         nodes = []
         tmp = self
         while tmp.parent:
@@ -35,9 +42,11 @@ class Node:
         nodes.reverse()
         return nodes
     
+
     def is_end(self):
         return self.board.is_game_over()
     
+
     def nodeRepresentation(self, c):
         n = self # BAD. I will change it later. TODO: change n.* to self.*
         moves = '/'.join([n.move for n in n.path()])
@@ -66,8 +75,8 @@ class Node:
                 f"[{lmoves} {moves}]"
                 # f"{n.board.fen()}"
 
-        if self.visits == 0:
-            return f"({self.visits-self.wins}/{self.visits}, {[str(m) for m in reversed(moves)]}, {self.board.fen()}) "
-        else:
-            return f"({1-self.wins/self.visits:.4f}, {self.wins/self.visits:.4f}, {self.visits-self.wins}/{self.visits}, {[str(m) for m in reversed(moves)]}, {self.board.fen()}) "
+        # if self.visits == 0:
+        #     return f"({self.visits-self.wins}/{self.visits}, {[str(m) for m in reversed(moves)]}, {self.board.fen()}) "
+        # else:
+        #     return f"({1-self.wins/self.visits:.4f}, {self.wins/self.visits:.4f}, {self.visits-self.wins}/{self.visits}, {[str(m) for m in reversed(moves)]}, {self.board.fen()}) "
     
