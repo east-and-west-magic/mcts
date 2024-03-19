5ba6efa7b098e1561fa2d998d5b091cc200d4e23
Amy <45153739+amtang325@users.noreply.github.com>
2024-03-19 10:14:58 -0400

    added threefold results

--- nn-simulation-testing/neural_net_simulation.py
+++ nn-simulation-testing/neural_net_simulation.py
@@ -119,6 +119,54 @@ def predictv3(boardFen, times, threshold):
             result['draw']+=1
     return result
 
+def predictv4(boardFen, times, threshold):
+    result = {'win': 0, 'lose': 0, 'perfect': 0, 'draw': 0}
+    nnue = cdll.LoadLibrary("./libnncpuprobe.so")
+    nnue.nncpu_init(b"nn-04cf2b4ed1da.nnue")
+    is_right_ending = True
+    for x in tqdm(range(times)):
+        board = chess.Board(boardFen)
+        while not board.is_game_over():
+            legal_moves = list(board.legal_moves)
+            probs = []
+            for m in legal_moves:
+                temp_board = board.copy()
+                temp_board.push(m)
+                fen = temp_board.fen()
+                score = nnue.nncpu_evaluate_fen(fen.encode())
+                probs.append(score)
+            probs = [prob - max(probs) for prob in probs]
+            denominator = 0
+            for p in probs:
+                denominator += math.e**p
+            probs[0] = math.e**probs[0] / denominator
+            for i in range(len(probs) - 1):
+                probs[i + 1] = math.e**probs[i + 1] / denominator + probs[i]
+            random_number = random.random()
+            move = None
+            if random_number <= threshold:
+                move = random.choice(legal_moves)
+            else:
+                rand = random.random()
+                for i in range(len(probs)):
+                    if (rand <= probs[i]):
+                        move = legal_moves[i]
+                        break
+            board.push(move)
+            if (not board.is_check()) and board.turn == chess.WHITE:
+                is_right_ending = False
+        res = board.result()
+        if res == '1-0':
+            result['win']+=1
+        elif res == '0-1':
+            result['lose']+=1
+        elif is_right_ending:
+            result['perfect']+=1
+            print('perfect')
+        else:
+            result['draw']+=1
+    return result
+
 def main():
     if False:
         with open('./results2/a.csv', 'w', newline='') as file:
@@ -221,7 +269,7 @@ def main():
                 writer.writerow([x + 1, dict['win'], dict['lose'], dict['draw'], simulations, fen, "0-1"])
             writer.writerow([""])
 
-    if True:
+    if False:
         with open('./results2/b.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Number", "Wins", "Losses", "Draws", "Simulations", "Fen"])
@@ -273,6 +321,40 @@ def main():
                 dict = predictv3(fen, simulations, 0.5)
                 writer.writerow([x + 1, dict['win'], dict['lose'], dict['draw'], simulations, fen, "0-1"])
             writer.writerow([""])
+
+    if True:
+        with open('./results3/threefold-4.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Number", "Wins", "Losses", "Perfect", "Draws", "Simulations", "Fen", "Random Threshold"])
+
+            simulations = 10000
+            times = 1
+            # FEN is threefold repetition
+            fen = "1r6/p1p3k1/4B1p1/5R2/8/1Pb5/q1P2PP1/3K3R w - - 1 27"
+            # file.flush()
+            for x in tqdm(range(times)):
+                dict = predictv4(fen, simulations, 0.2)
+                writer.writerow([x + 1, dict['win'], dict['lose'], dict['perfect'], dict['draw'], simulations, fen, 0.2])
+            writer.writerow([""])
+            file.flush()
+            # for x in tqdm(range(times)):
+            #     dict = predictv3(fen, simulations, 0.5)
+            #     writer.writerow([x + 1, dict['win'], dict['lose'], dict['draw'], simulations, fen, 0.5])
+            # writer.writerow([""])
+
+            # simulations = 10000
+            
+            for x in tqdm(range(times)):
+                dict = predictv4(fen, simulations, 0.8)
+                writer.writerow([x + 1, dict['win'], dict['lose'], dict['perfect'], dict['draw'], simulations, fen, 0.8])
+            writer.writerow([""])
+
+            # simulations = 10000
+            
+            # for x in tqdm(range(times)):
+            #     dict = predictv3(fen, simulations, 1)
+            #     writer.writerow([x + 1, dict['win'], dict['lose'], dict['draw'], simulations, fen, 1])
+            # writer.writerow([""])
 
 if __name__ == "__main__":
     main()
